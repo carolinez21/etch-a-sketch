b1c80484c2c62d25c9f9ec3eebcd8cb050f030bb
caroline <carzhang96@gmail.com>
2021-12-03 22:12:35 -0500

    add button for user to enter new grid size and redraw grid

--- js-file.js
+++ js-file.js
@@ -1,6 +1,33 @@
 const container = document.querySelector('.container');
-for(let i = 1; i <= 256; i++){
-    const square = document.createElement('div');
-    square.classList.add('grid-element');
-    container.appendChild(square);
+const button = document.querySelector('#btn');
+
+function drawGrid(numSquares){
+    container.style.gridTemplateColumns = `repeat(${numSquares}, ${800/numSquares}px)`;
+    let totalSquares = numSquares*numSquares;
+    for(let i = 1; i <= totalSquares; i++){
+        const square = document.createElement('div');
+        square.classList.add('grid-element');
+        container.appendChild(square);
+        square.addEventListener('mouseenter', e => {
+            square.style.backgroundColor = "pink";
+        })
+    }
 }
+drawGrid(16);
+
+
+function removeAllChildNodes(parent){
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+function createGrid(e) {
+    removeAllChildNodes(container);
+    let numSquares = prompt("Enter the number of squares you'd like per side of the grid (no more than 100)", "");
+    while(numSquares > 100){
+        numSquares = prompt("Sorry, please enter a number less than 100", "");
+    }
+    drawGrid(numSquares);
+}
+
+button.addEventListener('click', createGrid);
